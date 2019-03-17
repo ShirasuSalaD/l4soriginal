ff2fb9a4f96ec97f9b0df835f3ece62525ecaa0e
Hiroki Morimoto <hiroki@Hiroki-Morimoto.local>
2019-03-17 15:03:18 +0900

    fix: app.rb signinのget追加

--- app.rb
+++ app.rb
@@ -83,6 +83,10 @@ post '/signup' do
   redirect '/'
 end
 
+get '/signin' do
+  erb:sign_in
+end
+
 post '/signin' do
   user = User.find_by(name: params[:name])
   if user && user.authenticate(params[:password])
